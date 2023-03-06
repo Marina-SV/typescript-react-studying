00889cc2d8437a8a7595ddbe19364f5892ae2781
MarinaSenina <frau.senina32@yandex.ru>
2023-03-06 16:13:25 +0300

    Переиспользуемый компонент List

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,33 @@
 import React, {useEffect, useState} from 'react';
 import Card, {CardVariant} from "./components/Card";
 import axios from "axios";
-import {IUser} from "./types/types"
-import UserList from "./components/UserList";
+import {ITodo, IUser} from "./types/types"
+import List from "./components/List";
+import UserItem from "./components/UserItem";
+import TodoItem from "./components/TodoItem";
 
 
 const App = () => {
     const [users, setUsers] = useState<IUser[]>([])
+    const [todos, setTodos] = useState<ITodo[]>([])
 // почему не дублируется []? [[]]
     useEffect(() => {
         fetchUsers()
+        fetchTodos()
     }, [])
 
     async function fetchUsers() {
         const response = await axios.get<IUser[]>("https://jsonplaceholder.typicode.com/users")
-        console.log(response)
+        console.log(response) // Зачем методу get типизация?
         setUsers(response.data)
     }
+
+    async function fetchTodos() {
+        const response = await axios.get<ITodo[]>("https://jsonplaceholder.typicode.com/todos?_limit=10")
+        console.log(response) // Зачем методу get типизация?
+        setTodos(response.data)
+    }
+
 
     return (
         <div>
@@ -26,7 +37,12 @@ const App = () => {
                   onClick={(num) => console.log("yes", num)}>
                 <button>Кнопка</button>
             </Card>
-            <UserList users={users}/>
+            {/*<UserList users={users}/>*/}
+            <List items={users}
+                  renderItem={(user: IUser) => <UserItem user={user} key={user.id}/>}/>
+            <List items={todos}
+                  renderItem={(todo: ITodo) => <TodoItem todo={todo} key={todo.id}/>}/>
+            {/*Как понимает, что пропс users типа IUser[] ?*/}
         </div>
     );
 }
